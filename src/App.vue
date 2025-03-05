0533195f9bed71fb25b5064ad2e9849cc44d9cf0
唐雷 <2413081441@qq.com>
2025-03-05 14:50:46 +0800

    改进样式导入

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,7 @@
   <el-button type="primary">OK</el-button>
 </template>
 <script setup>
-import "~/styles/index.scss";
-import '~/composables';//初始化主题，默认自动主题
-import 'uno.css'
-import "~/styles/element/index.scss"
-import 'mapbox-gl/dist/mapbox-gl.css';
 // import rgyxWorkPoint from "~/components/rgyx-work-point.vue";
-import rgyxWorkPoint from "~/myComponents/人影/pages/synthesis.vue";
-import AircraftPos from "~/myComponents/websocket/aircraftPosition.vue";
 import rgyxWorkPoint from "~/myComponents/人影/pages/synthesis.vue";
 import AircraftPos from "~/myComponents/websocket/aircraftPosition.vue";
 </script>
