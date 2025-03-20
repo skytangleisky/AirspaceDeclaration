03d052a4e024b42c0e7d58fd98427c53705f0a2b
yhl <yhl_nsd@qq.com>
2025-03-20 10:06:41 +0800

    白色主题修改为黑色

--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import { createPinia } from "pinia"
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import piniaReset from './tools/piniaReset'
 import VueDOMPurifyHTML from 'vue-dompurify-html'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import "~/styles/index.scss";
+
 import '~/composables';//初始化主题，默认自动主题
 import 'uno.css'
-import "~/styles/element/index.scss"
+// import "~/styles/element/index.scss"
 import 'mapbox-gl/dist/mapbox-gl.css';
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
