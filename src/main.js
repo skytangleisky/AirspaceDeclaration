ab15bc77239f17bafe8c579e272b2ec94c1887d2
唐雷 <2413081441@qq.com>
2025-03-27 10:00:01 +0800

    将部分异步组件改成同步导入，优化主题相关代码

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import { defineAsyncComponent } from "vue";
+import App from "./App.vue"
 import dragable from './directive/dragable'
 import resize from './directive/resize'
 import { createApp } from 'vue'
-const App = defineAsyncComponent(()=>import("./App.vue"))
 import { createPinia } from "pinia"
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import piniaReset from './tools/piniaReset'
@@ -14,6 +13,7 @@ import "~/styles/index.scss";
 import '~/composables';//初始化主题，默认自动主题
 import 'uno.css'
 import 'mapbox-gl/dist/mapbox-gl.css';
+import "~/styles/element/index.scss";
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 pinia.use(piniaReset)
