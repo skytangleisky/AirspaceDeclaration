c6c025717ced5536159e261ccb289605851a6cb7
唐雷 <2413081441@qq.com>
2025-03-31 15:14:18 +0800

    关闭macos滚动回弹效果，control-pane使用el-scrollbar来滚动

--- src/myComponents/人影/pages/synthesis.vue
+++ src/myComponents/人影/pages/synthesis.vue
@@ -111,7 +111,9 @@
   <ColorSelector v-show="setting.人影.监控.showColorSelector !== -1" v-model:selectorColor="selectorColor" style="z-index: 2010;" @cancel="setting.人影.监控.showColorSelector=-1"></ColorSelector>
   <div ref="tweakPaneRef" class="tp-dfwv default hidden" data-pane-lighttheme style="z-index: 1;right:300px;"></div>
   <!-- <control-pane style="top:10px;right:10px;" :list="list" :theme="isDark?'default':'light'"></control-pane> -->
-  <control-pane style="top:10px;right:10px;" :list="list" theme="default"></control-pane>
+  <el-scrollbar style="position:absolute;right:0;height: 100%; width:fit-content;">
+    <control-pane style="position: relative;" :list="list" theme="default"></control-pane>
+  </el-scrollbar>
 </template>
 <script lang="ts" setup>
 import { watch, ref, reactive, computed,onMounted, onBeforeUnmount, toRefs, defineAsyncComponent } from "vue";
